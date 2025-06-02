f9ae8ee44d5c5bc3151f5036c01ef3fbf23ea8bf
Thapson Nyirenda <thapsonknyirenda@gmail.com>
2025-06-02 15:02:05 +0200

    Initial commit

---
+++ fast-api/main.py
@@ -0,0 +1,46 @@
+from uuid import UUID, uuid4
+from fastapi import FastAPI
+from models.User import User, Gender, Role
+
+app = FastAPI()
+
+db: list[User]=[
+    User(
+        id=UUID("c79736f9-77b7-4e70-8349-8bfbf0f4dc8a"),
+        first_name="John",
+        middle_name="Doe",
+        last_name="Doe",
+        gender=Gender.male,
+        roles=[Role.student],
+    ),
+    User(
+        id=UUID("d5943b83-bcb3-4271-aab6-f26a681c1250"),
+        first_name="Thapson",
+        middle_name="Kapembe",
+        last_name="Nyirenda",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user],
+    ),
+]
+
+@app.get("/")
+def root():
+    return {"message": "Hello, Thapson!"}
+
+@app.get("/api/v1/users")
+async def get_users():
+    return db
+
+@app.post("/api/v1/users")
+async def create_user(user: User):
+    db.append(user)
+    return ("user created successfully", user.id)
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return {"message": "User deleted successfully"}
+        else:
+            return {"error": "User not found"}
---
+++ fast-api/models/User.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from typing import Optional
+from pydantic import BaseModel
+from uuid import UUID, uuid4
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+    
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
+    student = "student"
+class User(BaseModel):
+    id: Optional[UUID] = uuid4()
+    first_name: str
+    middle_name: Optional[str]
+    last_name: str
+    gender: Gender
+    roles: list[Role]
